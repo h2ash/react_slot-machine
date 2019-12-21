50eaa0a97e4b2148749a5ae3efab5e6cce920fbd
h2ash <hun22ash@gmail.com>
2019-12-21 19:33:45 +0200

    Updates

---
+++ precache-manifest.848019c6ca2e96c6dfe09442384d2ed9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e934ea4f9bbd982a0483fe07ec15aaf",
+    "url": "/h2ash/react_slot-machine/index.html"
+  },
+  {
+    "revision": "a4a48055c13c293791f6",
+    "url": "/h2ash/react_slot-machine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a4a21873f4ae905e2e6",
+    "url": "/h2ash/react_slot-machine/static/js/2.881d1e0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/h2ash/react_slot-machine/static/js/2.881d1e0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4a48055c13c293791f6",
+    "url": "/h2ash/react_slot-machine/static/js/main.bd8d9cdb.chunk.js"
+  },
+  {
+    "revision": "0e2827d0a523d0ffcbec",
+    "url": "/h2ash/react_slot-machine/static/js/runtime-main.418f7cdb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/h2ash/react_slot-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
